a14ade1ce3f3d63e4852c880135e6f1642bbd859
ff137 <ff137@proton.me>
2024-07-10 22:34:19 +0300

    :art:

--- example.py
+++ example.py
@@ -8,11 +8,12 @@ from luno_openapi.models.ticker import Ticker
 api_key_id = os.environ.get("API_KEY_ID")
 api_key_secret = os.environ.get("API_KEY_SECRET")
 
-client = LunoClient(api_key_id=api_key_id, api_key_secret=api_key_secret)
-
 
 async def run():
-    result: ListTickersResponse = await client.market_api.get_tickers()
+    async with LunoClient(
+        api_key_id=api_key_id, api_key_secret=api_key_secret
+    ) as client:
+        result: ListTickersResponse = await client.market_api.get_tickers()
 
     tickers: List[Ticker] = result.tickers or []
 
@@ -20,4 +21,4 @@ async def run():
         print(f"{ticker=}")
 
 
-asyncio.get_event_loop().run_until_complete(run())
+asyncio.new_event_loop().run_until_complete(run())
